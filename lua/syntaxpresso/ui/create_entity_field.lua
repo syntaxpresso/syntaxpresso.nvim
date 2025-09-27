2a0746b7a987a98de60cd1be22bc428ca9b3136b
André Luís <zandreluisos@gmail.com>
2025-09-27 16:01:13 -0300

    fix: arrange buttons in the same line

--- lua/syntaxpresso/ui/create_entity_field.lua
+++ lua/syntaxpresso/ui/create_entity_field.lua
@@ -1,7 +1,8 @@
 local n = require("nui-components")
 local basic_field = require("syntaxpresso.ui.basic_field")
-local basic_field_signal = basic_field.create_signal()
 local select_one = require("syntaxpresso.ui.select_one")
+
+local renderer = n.create_renderer({ height = 7 })
 
 local signal = n.create_signal({
   active_tab = "tab-1",
@@ -11,16 +12,8 @@ local signal = n.create_signal({
   confirm_button_hidden = true
 })
 
-local renderer = n.create_renderer({ flex = 1, height = 7 })
+local basic_field_signal = basic_field.create_signal()
 
-local function render_field_category_selector()
-  local data = {
-    n.node({ text = "Basic Field", is_done = true, id = "basic" }),
-    n.node({ text = "Enum Field", is_done = false, id = "enum" }),
-    n.node({ text = "ID Field", is_done = false, id = "id" }),
-  }
-  return select_one.render_component(3, "Category", data, "field_category", _signal, true)
-end
 
 local function render_main_title()
   return n.paragraph({
@@ -32,8 +25,18 @@ local function render_main_title()
   })
 end
 
+local function render_field_category_selector()
+  local data = {
+    n.node({ text = "Basic Field", is_done = true, id = "basic" }),
+    n.node({ text = "Enum Field", is_done = false, id = "enum" }),
+    n.node({ text = "ID Field", is_done = false, id = "id" }),
+  }
+  return select_one.render_component(3, "Category", data, "field_category", signal, true)
+end
+
 local function render_next_button()
   return n.button({
+    flex = 1,
     label = "Next ->",
     align = "center",
     global_press_key = "<C-CR>",
@@ -50,6 +53,7 @@ end
 
 local function render_previous_button()
   return n.button({
+    flex = 1,
     label = "Previous <-",
     align = "center",
     global_press_key = "<C-CR>",
@@ -64,21 +68,62 @@ local function render_previous_button()
   })
 end
 
+local function render_confirm_button()
+  return n.button({
+    flex = 1,
+    label = "Confirm",
+    align = "center",
+    global_press_key = "<C-CR>",
+    on_press = function()
+      local category = signal.field_category:get_value()
+      if category == "basic" then
+        local result = basic_field.get_field_data(basic_field_signal)
+        vim.call("CreateBasicEntityFieldCallback", result)
+        renderer:close()
+        -- elseif category == "enum" then
+        --   local result = enum_field.get_field_data(enum_field_signal)
+        --   vim.call("CreateEnumEntityFieldCallback", result)
+        --   renderer:close()
+        -- elseif category == "id" then
+        --   local result = id_field.get_field_data(id_field_signal)
+        --   vim.call("CreateIdEntityFieldCallback", result)
+        --   renderer:close()
+      end
+    end,
+    hidden = signal.confirm_button_hidden,
+  })
+end
 
-renderer:render(n.tabs(
-  { active_tab = signal.active_tab },
-  render_main_title(),
-  n.tab(
-    { id = "tab-1" },
-    render_field_category_selector()
-  ),
-  n.tab(
-    { id = "tab-2" },
-    basic_field.render_component(basic_field_signal)
-  ),
-  n.tab(
-    { id = "tab-3" }
-  ),
-  render_next_button(),
-  render_previous_button()
-))
+local function render_field_component()
+  local category = signal.field_category:get_value()
+  if category == "basic" then
+    return basic_field.render_component(basic_field_signal)
+  else
+    return basic_field.render_component(basic_field_signal)
+  end
+end
+
+local function render_component()
+  return n.tabs(
+    { active_tab = signal.active_tab },
+    render_main_title(),
+    n.tab(
+      { id = "tab-1" },
+      render_field_category_selector()
+    ),
+    n.tab(
+      { id = "tab-2" },
+      render_field_component()
+    ),
+    n.tab(
+      { id = "tab-3" }
+    ),
+    n.columns(
+      { flex = 0 },
+      render_next_button(),
+      render_previous_button(),
+      render_confirm_button()
+    )
+  )
+end
+renderer:render(render_component())
