9a1821f16cd1dc5d151be4c447f74912861b875e
André Luís <zandreluisos@gmail.com>
2025-11-04 18:34:56 -0300

    feat(commands): add get_all_superclasses command and refactor package command logic

--- lua/syntaxpresso/commands/get_all_superclasses.lua
+++ lua/syntaxpresso/commands/get_all_superclasses.lua
@@ -2,15 +2,13 @@ local command_runner = require("syntaxpresso.utils.command_runner")
 
 local M = {}
 
----Get all packages from the current working directory
+---Get all superclasses from the current working directory
 ---@param cwd string|nil The working directory path (defaults to current working directory)
----@param source_directory string|nil The source directory type ("main" or "test", defaults to "main")
 ---@param callback fun(response: table|nil, error: string|nil) Callback function
 ---@param options table|nil Optional settings
-function M.get_all_packages(cwd, source_directory, callback, options)
-	-- Set default values
+function M.get_all_superclasses(cwd, callback, options)
+	-- Set default value
 	local actual_cwd = cwd or vim.fn.getcwd()
-	local actual_source_directory = source_directory or "main"
 	-- Validate callback
 	if not callback or type(callback) ~= "function" then
 		error("Callback function is required")
@@ -18,16 +16,15 @@ function M.get_all_packages(cwd, source_directory, callback, options)
 	-- Build arguments
 	local args = {
 		cwd = actual_cwd,
-		["source-directory"] = actual_source_directory,
 	}
 	-- Execute command
-	command_runner.execute("get-all-packages", args, callback, options)
+	command_runner.execute("get-all-superclasses", args, callback, options)
 end
 
----Simplified version that gets all packages from current directory with default options
+---Simplified version that gets all superclasses from current directory
 ---@param callback fun(response: table|nil, error: string|nil) Callback function
-function M.get_all_packages_simple(callback)
-	M.get_all_packages(nil, nil, callback, nil)
+function M.get_all_superclasses_simple(callback)
+	M.get_all_superclasses(nil, callback, nil)
 end
 
 return M
--- lua/syntaxpresso/commands/init.lua
+++ lua/syntaxpresso/commands/init.lua
@@ -1,7 +1,9 @@
 local get_all_packages = require("syntaxpresso.commands.get_all_packages")
+local get_all_superclasses = require("syntaxpresso.commands.get_all_superclasses")
 
 local M = {}
 
 M.get_all_packages = get_all_packages
+M.get_all_superclasses = get_all_superclasses
 
 return M
